020a1848b2c03e44cd70c6d1ad3f69e001addfd1
Alexander J. Renner <arenner38@yahoo.com>
2016-05-07 16:42:47 -0500

    Added preliminary questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,14 +1,55 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees are we registering?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+def vampire_check
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread = gets.chomp
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "Would you like to enroll in the copany's health insurance?"
-insurance = gets.chomp
+	if name.downcase == "drake cula" || name.downcase == "tu fang"
+		puts "Definitely a vampire!"
+		break
+	end
 
-puts "It is good to meet you #{name}!"
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+	garlic_bread = gets.chomp
+
+	if garlic_bread.downcase == "yes"
+		garlic = true
+	else
+		garlic = false
+	end
+
+	puts "Would you like to enroll in the copany's health insurance? (yes or no)"
+	insure = gets.chomp
+
+	if insure.downcase == "yes"
+		insurance = true
+	else
+		insurance = false
+	end
+
+	puts "It is good to meet you #{name}!"
+
+	puts 
+
+	if age >= 90 && !garlic && !insurance
+		puts "Definitely a vampire!"
+	elsif age >= 90 && !garlic || !insurance
+		puts "Almost certainly a vampire."
+	elsif age < 90 && !garlic || !insurance
+		puts "Probably a vampire."
+	elsif age < 90 && garlic && insurance
+		puts "Not a vampire!"
+	else
+		puts "Results inconclusive..."
+	end
+
+end
+
+employees.times do
+	vampire_check
+end
