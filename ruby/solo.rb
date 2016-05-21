58483071dbff45065028f6d64fd884abba61f357
Alexander J. Renner <arenner38@yahoo.com>
2016-05-21 00:12:37 -0500

    created pseudocode and SpaceShip class

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+# create space ship class
+# create ship name, fuel type, fuel capacity, current fuel and velocity attributes
+# create model update, fuel usage and velocity change methods
+# call space ship stats in a string
+
+class SpaceShip
+end
