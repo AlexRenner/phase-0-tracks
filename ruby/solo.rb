4407d426d58baf62dca609109dacf8085e5412c2
Alexander J. Renner <arenner38@yahoo.com>
2016-05-21 00:25:21 -0500

    Added attributes inside inilitize method

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -4,4 +4,15 @@
 # call space ship stats in a string
 
 class SpaceShip
+	attr_reader :ship_name, :fuel_type, :fuel_capacity
+	attr_accessor :ship_model, :current_fuel, :velocity
+
+	def initialize(model, fuel, capacity)
+		@ship_name = "Orion"
+		@ship_model = model
+		@fuel_type = "Combustables"
+		@fuel_capacity = capacity
+		@current_fuel = fuel
+		@velocity = 0
+	end
 end
