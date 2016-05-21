2582fdf4b1f73be4d08aaf07eb6d0d323faf4935
Alexander J. Renner <arenner38@yahoo.com>
2016-05-21 00:52:18 -0500

    created class methods and armada array

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -7,8 +7,8 @@ class SpaceShip
 	attr_reader :ship_name, :fuel_type, :fuel_capacity
 	attr_accessor :ship_model, :current_fuel, :velocity
 
-	def initialize(model, fuel, capacity)
-		@ship_name = "Orion"
+	def initialize(name, model, fuel, capacity)
+		@ship_name = name
 		@ship_model = model
 		@fuel_type = "Combustables"
 		@fuel_capacity = capacity
@@ -16,9 +16,25 @@ class SpaceShip
 		@velocity = 0
 	end
 
+	def change_velocity(velocity, usage)
+		@velocity = velocity
+		@current_fuel = @current_fuel - usage
+	end
+
+	def check_fuel
+		fuel_level = @fuel_capacity * (@current_fuel.to_f * 0.01)
+	end
+
 end
 
-Orion = SpaceShip.new("X21", 100, 1000)
+armada = []
 
-puts "The #{Orion.ship_name}-#{Orion.ship_model} uses #{Orion.fuel_type.downcase}.
-It holds #{Orion.fuel_capacity}lbs of fuel and is #{Orion.current_fuel}\% full."
+armada << SpaceShip.new("Orion", "X21", 100, 1000)
+
+puts ' '
+puts "The #{orion.ship_name}-#{orion.ship_model} uses #{orion.fuel_type.downcase}.
+It holds #{orion.fuel_capacity}lbs of fuel and is #{orion.current_fuel}\% full."
+puts ' '
+
+orion.current_fuel = 80
+puts "#{orion.ship_name} has #{orion.check_fuel} lbs of fuel left."
