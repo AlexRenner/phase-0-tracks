8f01fc53b520c122ae52798568449c8a145c7681
Alexander J. Renner <arenner38@yahoo.com>
2016-05-22 18:59:48 -0500

    Created flight content and updated shout file

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,19 @@
+# module Shout
+	
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!" + " Yipeeee! :-)"
+# 	end
+
+# end
+
+# p Shout.yelling_happily("I won!")
+
+# p Shout.yell_angrily("What is going on?")
+
 module Shout
 	
 	def yell_angrily(words)
@@ -14,8 +30,14 @@ class Person
 	include Shout
 end
 
+class ConfusedPersons
+	include Shout
+end
+
 happy = Person.new
 p happy.yelling_happily("I won!")
+p happy.yell_angrily("Why am I excited?")
 
-angry = Person.new
-p angry.yell_angrily("What is going on?")
+confused = ConfusedPerson.new
+p confused.yell_angrily("What is going on?")
+p confused.yelling_happily("Not sure what's going on but it's fun")
