a4622956ac9f4a59420496db0a9c693a490a32e9
Alexander J. Renner <arenner38@yahoo.com>
2016-05-27 18:09:11 -0500

    added loop and if statement

--- js/explore.js
+++ js/explore.js
@@ -1,9 +1,25 @@
 // declare string
 // write loop
-// print each letter from last to first
+// push each letter from last to first
+// join array into new string
 
 var str_fwd = "In reverse!";
+var letters = [];
 
-for (var i = 0; i < str_fwd.length; i++) {
-	console.log(str_fwd[i]);
+function reverse(str_fwd) {
+
+	for (var i = str_fwd.length; i > 0; i--) {
+		letters.push(str_fwd[i-1]);
+
+	}
+}
+
+reverse(str_fwd);
+var str_bkwd = letters.join("");
+
+if (str_fwd != null) {
+	console.log(str_bkwd);
+}
+else {
+	console.log("The string is empty!");
 }
