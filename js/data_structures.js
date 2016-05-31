573764a5483b9f69b5ca0c5731caf2a0626733de
Alexander J. Renner <arenner38@yahoo.com>
2016-05-30 19:32:00 -0500

    declared variables and added pseudocode

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+var colors = ["blue", "green", "yellow", "orange"];
+var names = ["joe", "bob", "jane", "doe"];
+
+colors.push("red");
+names.push("jill");
+
+// declare a method that takes two arrays
+// declare new object
+// if statement to compare array.length
+// write loop to push name=>colors into new object
+// else yell at user
