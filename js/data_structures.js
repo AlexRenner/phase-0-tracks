e1f16af2e245c8d00b7b2f975944d993b9e0c40b
Alexander J. Renner <arenner38@yahoo.com>
2016-05-30 20:03:16 -0500

    Wrote object set for car creation

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,3 +9,32 @@ names.push("jill");
 // if statement to compare array.length
 // write loop to push name=>colors into new object
 // else yell at user
+
+function push_info(name_array, color_array) {
+  var horseData = {};
+  if (name_array.length === color_array.length) {
+   for (var i = 0; i < name_array.length; i++) {
+     horseData[name_array[i]] = color_array[i];
+     // console.log(horseData);
+   }
+  } else {
+    console.log("Lengths of arrays don't match!");
+  }
+};
+
+console.log(push_info(names, colors));
+
+function car(make, model, year) {
+  
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  
+  this.revItUp = function() {console.log("VROOOOOOOM");};
+  
+  console.log("CAR IS BEING BUILT...");
+};
+
+var paul = new car("chevy", "impala", 2007);
+console.log(paul);
+paul.revItUp();
