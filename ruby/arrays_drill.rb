a61f85bb398c14044896b9ec0ff5df22dcac098e
Alexander J. Renner <arenner38@yahoo.com>
2016-06-03 14:54:30 -0500

    Did release 1: steps 1-3

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,47 +1,15 @@
-def array_method(a,b,c)
- build_array = [a,b,c]
- p build_array
-end
+new_array = []
 
-def array_method2(arr, value)
- 	arr << value 
- 	p arr 
-end
-
-array_method2([1,2,3], 4)
-array_method2(["a","b","c"], "d")
-array_method2(["$@$%", "#@%*", "!@$^" ], "$%^&")
-
-empty_array = []
-p empty_array 
-
-empty_array << "one"
-empty_array << "two"
-empty_array << "three"
-empty_array << "four"
-empty_array << "five"
-p empty_array 
-
-
-empty_array.delete_at(2)
-p empty_array
-
-empty_array.insert(2, "six")
-p empty_array 
-
-empty_array.delete("one") 
-p empty_array
-
-if empty_array.include?("six")
- puts "This array includes six!"
-else
- puts "This array does not include six!"
-end
-
-new_array = ["dogs","cats","rabbits","hamsters", "octopus"]
 p new_array
 
-added_arrays = new_array + empty_array 
-p added_arrays
+new_array << "item1"
+new_array << "item2"
+new_array << "item3"
+new_array << "item4"
+new_array << "item5"
 
-array_method(1,2,3)
+p new_array
+
+new_array.delete_at(2)
+
+p new_array
