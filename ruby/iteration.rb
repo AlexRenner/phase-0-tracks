c7663f095cb0a93bcbf6953d5565748a9dd82f7c
Alexander J. Renner <arenner38@yahoo.com>
2016-06-04 18:13:52 -0500

    Created delete and filter methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -23,3 +23,32 @@ wine_for_food.each {|meat, wine| puts "#{wine.capitalize} wine goes with #{meat}
 
 new_hash = Hash[wine_for_food.map { |meat, wine| [meat, wine.capitalize] } ]
 p new_hash
+
+def delete_item (array)
+	array.each do |item|
+		if item.is_a?(String) && item.include?(".")
+			array.delete(item)
+		else
+			puts "Nothing to delete"
+		end
+	end
+end
+
+test_array = ["a", 11, false, nil]
+delete_item(soda_flavors)
+p soda_flavors
+delete_item(test_array)
+p test_array
+
+def filter_hash(hash)
+	hash.each do |meat, wine|
+		if wine.downcase == "white"
+			# p hash
+		else 
+			hash.delete(meat)
+		end
+	end
+end
+
+filter_hash(wine_for_food)
+p wine_for_food
