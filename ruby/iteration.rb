cf92b9ec4e9a570ad66f497a316c8ebfbc468e90
Alexander J. Renner <arenner38@yahoo.com>
2016-06-04 15:10:25 -0500

    Iterated with .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -2,10 +2,24 @@ soda_flavors = ["Pepsi", "Coke", "Dr. Pepper", "Sprite"]
 wine_for_food = {
 	:poultry => "white",
 	:beef => "red",
-	:pork => "depends",
+	:pork => "either",
 	:fish => "white"
 }
 
 soda_flavors.each do |flavor|
 	puts "I like #{flavor}!"
 end
+
+soda_flavors.map do |flavor| 
+	flavor = flavor + " cola"
+	p flavor
+end
+
+soda_flavors.map! { |flavor| flavor.downcase }
+
+p soda_flavors
+
+wine_for_food.each {|meat, wine| puts "#{wine.capitalize} wine goes with #{meat}."}
+
+new_hash = Hash[wine_for_food.map { |meat, wine| [meat, wine.capitalize] } ]
+p new_hash
