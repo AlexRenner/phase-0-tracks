0529c51d4d22c7dab0774d9a2201189c23b0652b
Alexander J. Renner <arenner38@yahoo.com>
2016-06-05 21:55:46 -0500

    created hash and alias method

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,49 +1,28 @@
-# create method for making fake name
+# declare neccesary variables
+# define method that takes an array
+# prompt user for their name
+# break name into array
+# iterate through array
+# write fix for edge cases and irregular characters
+# push to hash (alias => real name)
 
-def fake_name(name)
-		name_array = name.split(' ')
-		new_name = name_array[1] + ' ' + name_array[0]
-		scrambled_name = new_name.downcase.gsub(/[aeiou]/, 'a' => 'e', 'e' => 'i', 'i' => 'o', 'o' => 'u', 'u' => 'a')
-		scrambled_again = scrambled_name.gsub(/[bcdfghjklmnpqrstvwxyz]/, 'b' => 'c', 'c' => 'd', 'd' => 'f', 'f' => 'g', 'g' => 'h', 'h' => 'j', 'j' => 'k', 'k' => 'l', 'l' => 'm', 'm' => 'n', 'n' => 'p', 'p' => 'q', 'q' => 'r', 'r' => 's', 's' => 't', 't' => 'v', 'v' => 'w', 'w' => 'x', 'x' => 'y', 'y' => 'z', 'z' => 'a')
-		scrambled_array = scrambled_again.split(' ')
-		final_name = scrambled_array[0].capitalize + ' ' + scrambled_array[1].capitalize
-		return final_name
-end
+alias_hash = Hash.new
 
-$spy_name = nil
-$name_equivelents = Hash.new
-
-class NameGenerator
-	
-	until $spy_name == "done"
-# prompt user for their spy name
-		puts "What is your secret spy first and last name? (type \"done\" to cancel)" 
-		print '>'
-		$spy_name = gets.chomp
-		puts ' '
-		if $spy_name.downcase == "done"
-			$name_equivelents.each do |name, new_name|
-				puts "#{name}'s fake name is #{new_name}."
-			end
-			puts "Don't tell anyone!"
-			exit(0)
+def alias_maker
+	name = nil
+	until name.downcase == "quit"
+		puts "Hello! What name would you like to alias? (Or type \"quit\" to exit)"
+		name = gets.chomp
+		if name.downcase == "quit"
+			exit_method(alias_hash)
 		else
-			alias_name = fake_name($spy_name)
+			name_array = name.split(' ')
+			p name_array
 		end
-		puts "Your new super top secret alias is #{alias_name}!"
-		puts ' '
-		
-		$name_equivelents[$spy_name] = alias_name
 	end
 end
 
-NameGenerator
-
-#  Some stuff that I tried and I left for future consideration
-
-#exploded_name = new_name.split(//)
-#	exploded_name.each do |x|
-#		vowels = 'aeiou'
-#		consonants = 'bcdfghjklmnpqrstvwxyz'
-#		index = 0
-#		while index
+def exit_method(hash)
+	hash.each {|code_name, name| puts "#{code_name} is the alias of #{name}."}
+	exit(0)
+end
