8712d5d3016e3fc86f14e20bb4a0143acbafe880
Alexander J. Renner <arenner38@yahoo.com>
2016-06-10 11:55:03 -0500

    Added generator method and .times loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,7 +6,7 @@ class Santa
 		puts "Initializing Santa..."
 		@gender = gender
 		@ethnicity = ethnicity
-		@age = 0
+		@age = rand(140)
 		@reindeer_ranking = [
 			"Rudolph", "Dasher", "Dancer",
 			"Prancer", "Vixen", "Comet",
@@ -41,19 +41,25 @@ class Santa
 end
 
 santa_array = []
-genders = ["agender", "female", "bigender", "male"]
-ethnicities = ["black", "latino", "white", "asian",
-	"indian", "middle-eastern", "part dragon"]
 
-santa_test = Santa.new(genders[3], ethnicities[0])
-santa_array << santa_test
+# santa_test = Santa.new(genders[3], ethnicities[0])
+# santa_array << santa_test
 
-santa_test.speak
-santa_test.eat_milk_and_cookies("no bake")
-187.times { santa_test.celebrate_birthday }
-santa_test.get_mad_at("Vixen")
-p santa_test.age
-p santa_test.ethnicity
-santa_test.ethnicity = "couch"
+# santa_test.speak
+# santa_test.eat_milk_and_cookies("no bake")
+# 187.times { santa_test.celebrate_birthday }
+# santa_test.get_mad_at("Vixen")
+# p santa_test.age
+# p santa_test.ethnicity
+# santa_test.ethnicity = "couch"
+
+def santa_generator(array)
+	genders = ["agender", "female", "bigender", "male"]
+	ethnicities = ["black", "latino", "white", "asian",
+		"indian", "middle-eastern", "part dragon"]
+	array << Santa.new(genders.sample, ethnicities.sample)
+end
+
+10.times {santa_generator(santa_array)}
 
 p santa_array
