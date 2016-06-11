8314a5771ff64b3b3b90281995b53cffc7f24552
Alexander J. Renner <arenner38@yahoo.com>
2016-06-11 16:14:00 -0500

    Created pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,27 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]break string into array items
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash of item => quantity
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: take list and item push item into list
+# output: hash pair (or full hash)
+
+# Method to remove an item from the list
+# input: identify its key 
+# steps:delete the key (deletes value too)
+# output: updated list
+
+# Method to update the quantity of an item
+# input: key and its new value
+# steps:identify key, set key equal to new quantity
+# output: upadated pair
+
+# Method to print a list and make it look pretty
+# input: the list's hash
+# steps: puts a sentence with dressed up hash
+# output: nice string for the user
