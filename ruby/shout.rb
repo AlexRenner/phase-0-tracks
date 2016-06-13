9685b1e75ea44ab7681f59b8e5628380cbbd7586
Alexander J. Renner <arenner38@yahoo.com>
2016-06-12 19:44:26 -0500

    Added classes and class instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,43 @@
+# module Shout
+
+#   def self.yell_angrily(words)
+#     p words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#   	p words + "! Yay!"
+#   end
+
+# end
+
+# Shout.yell_happily("What?")
+# Shout.yell_angrily("What?")
+
 module Shout
 
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     p words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
   	p words + "! Yay!"
   end
 
 end
 
-Shout.yell_happily("What?")
-Shout.yell_angrily("What?")
+class SportsFan
+	include Shout
+end
+
+class Friend
+	include Shout
+end
+
+student = SportsFan.new
+john = Friend.new
+
+student.yell_happily("What?")
+student.yell_angrily("Why?")
+
+john.yell_happily("Seriously?")
+john.yell_angrily("Since when?")
