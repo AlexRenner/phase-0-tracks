d083afc9df788e6af7a9a0db18f1e24438a25c47
Alexander J. Renner <arenner38@yahoo.com>
2016-06-14 13:17:19 -0500

    Wrote pseudocode and test variable

---
+++ js/algos.js
@@ -0,0 +1,8 @@
+// create a test array
+// declare the function
+// declare initial length variable
+// compare each array item to the variable
+// update longest item only if the current array item is longer
+// (if I can figure it out) create a new array if multiple items are the longest
+
+var test_array = ["tiny", "short", "longer", "nope"];
