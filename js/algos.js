6502361a9d823e38ace6b1ec62881688aad8121c
Alexander J. Renner <arenner38@yahoo.com>
2016-06-14 13:37:04 -0500

    Created length check function with array addition

--- js/algos.js
+++ js/algos.js
@@ -6,3 +6,22 @@
 // (if I can figure it out) create a new array if multiple items are the longest
 
 var test_array = ["tiny", "short", "longer", "nope"];
+var longest = "";
+var longest_array = [];
+
+var test_array2 = ["here's", "another", "string", "array", "to test", "Wanted to test another"];
+
+function length_check(string) {
+	if (string.length > longest.length) {
+		longest = string;
+		longest_array = [string];
+	} else if (string.length === longest.length) {
+		longest_array.push(string);
+	}
+	console.log(longest_array);
+}
+
+test_array.forEach(length_check);
+console.log("Now let's test the second array:");
+var longest = ""; //had to clear variable to prevent including previous longest
+test_array2.forEach(length_check);
