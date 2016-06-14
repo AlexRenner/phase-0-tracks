2da1fbad66988ab1d9ddb5a601e980c313326ebd
Alexander J. Renner <arenner38@yahoo.com>
2016-06-14 12:46:44 -0500

    Added test code and a second function

--- js/explore.js
+++ js/explore.js
@@ -6,13 +6,34 @@
 // print new string and old string to console (make it pretty)
 
 var my_string = "Take it back now y'all!"
+var test_string = ""
 
 function reverse(string) {
 	var letter_array = [];
 
-	for (var index = string.length; index > 0; i--) {
+	for (var index = string.length; index > 0; index--) {
 		letter_array.push(string[index-1]);
 	}
 
-	console.log(letter_array)
+	// console.log(letter_array);
+	reversed_string = letter_array.join("");
+	// console.log(string);
+	//return string; can't seem to get string to return and update my_string
+	// my_string = letter_array.join("");
+	make_it_pretty(string, reversed_string)
+	// decided to just call function within a function instead
 };
+
+function make_it_pretty(fwd_string, bwd_string) {
+	if (fwd_string.length > 0) {
+		console.log(fwd_string + " backwards would look like this: ");
+		console.log(bwd_string);
+	} else {
+		console.log("I can't work with nothing here!");
+	}
+};
+
+reverse(my_string);
+reverse(test_string);
+
+// console.log(my_string);
