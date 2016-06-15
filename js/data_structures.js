4d02feec3cdf6750ddce7d893a51223ad887d6ed
Alexander J. Renner <arenner38@yahoo.com>
2016-06-15 11:02:50 -0500

    Added builder function and driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -6,3 +6,20 @@ names.push("Jumper");
 
 var horse = {name: 'lightning', age: 32, color: 'palomino'};
 
+var newHorses = {};
+
+function horseBuilder(name, color) {
+ if (name.length === color.length) {
+   for (var i = 0; i < name.length; i++) {
+   newHorses[name[i]] = color[i];
+     console.log(newHorses);
+   }
+ } else {
+   console.log("Array's do not match");
+ }
+};
+
+
+horseBuilder(names, colors);
+// Make a loop with a counter that adds the arrays index that is equal to the current counter to 
+// both the key and value of the newHorse object
