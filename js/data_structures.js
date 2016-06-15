1d56351c85ad466416e4cc5470dbb0b08566e02a
Alexander J. Renner <arenner38@yahoo.com>
2016-06-15 11:33:27 -0500

    Added Car object and print function with driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -19,7 +19,29 @@ function horseBuilder(name, color) {
  }
 };
 
-
 horseBuilder(names, colors);
 // Make a loop with a counter that adds the arrays index that is equal to the current counter to 
 // both the key and value of the newHorse object
+
+function Car(make, model, year) {
+  
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  
+  console.log("Constructing car:", this);
+  
+  
+};
+
+var paul = new Car("Chevy", "Impala", 2007);
+var sean = new Car("Ford", "Focus", 2013);
+
+function currentCar(userName, instance) {
+  console.log(userName + "'s " + instance.make +" "+ instance.year +" "+ instance.model + " is his current car.");
+};
+
+currentCar("Alex", paul);
+currentCar("Sean", sean);
+// console.log("Alex's " + paul.make +" "+ paul.year +" "+ paul.model + " is his current car.");
+// console.log(paul);
