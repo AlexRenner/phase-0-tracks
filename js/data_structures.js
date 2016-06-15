f9549fb3a98c62bf690ecf1692f672d5bafb11a2
Alexander J. Renner <arenner38@yahoo.com>
2016-06-15 10:37:50 -0500

    Created js file and arrays

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,46 +1,8 @@
-var colors = ["blue", "green", "yellow", "orange"];
-var names = ["joe", "bob", "jane", "doe"];
+var colors = ["blue", "green", "orange", "yellow"];
+var names = ["Charlie", "Mr. Ed", "Glue", "Crazy Horse"];
 
-colors.push("red");
-names.push("jill");
+colors.push("Red");
+names.push("Jumper");
 
-// declare a method that takes two arrays
-// declare new object
-// if statement to compare array.length
-// write loop to push name=>colors into new object
-// else yell at user
+var horse = {name: 'lightning', age: 32, color: 'palomino'};
 
-function push_info(name_array, color_array) {
-  var horseData = {};
-  if (name_array.length === color_array.length) {
-   for (var i = 0; i < name_array.length; i++) {
-     horseData[name_array[i]] = color_array[i];
-     // console.log(horseData);
-   }
-  } else {
-    console.log("Lengths of arrays don't match!");
-  }
-};
-
-console.log(push_info(names, colors));
-
-function car(make, model, year) {
-  
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  
-  this.revItUp = function() {console.log("VROOOOOOOM");};
-  
-  console.log("CAR IS BEING BUILT...");
-};
-
-var paul = new car("chevy", "impala", 2007);
-console.log(paul);
-paul.revItUp();
-
-var tabatha = new car("chevy", "cobalt", 2009);
-console.log(tabatha);
-
-var pam = new car("chevy", "malibu", 1999);
-console.log(pam);
